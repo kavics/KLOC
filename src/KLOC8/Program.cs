731afaeefb091a25d28a44946c38e657add7849d
kavics <kavics@sensenet.com>
2024-12-16 23:25:57 +0100

    Feat/kloc per filetype (#16)
    * Kloc per filetype.
    
    * Rewrite to .NET 8
    
    * Test project rewritten to .NET 8 but commented out.

---
+++ src/KLOC8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.CommandLine;
+using KLOC8;
+
+var rootCommand = new RootCommand();
+
+var pathArgument = new Argument<string>("path", "Location of source code directory (required)");
+rootCommand.AddArgument(pathArgument);
+
+var isContainerOption = new Option<bool>(name: "--isContainer", description: "Enumerate and count sub-directories and displays a name-count pairs in a table.");
+isContainerOption.AddAlias("-c");
+rootCommand.Add(isContainerOption);
+
+var fileTypesOption = new Option<string>(name: "--fileTypes", description: "Whitelist of enabled file-extensions (e.g. -t .cs,.sql).");
+fileTypesOption.AddAlias("-t");
+rootCommand.Add(fileTypesOption);
+
+rootCommand.SetHandler((path, isContainer, fileTypes) =>
+{
+    var command = new KlocCommand();
+    command.Execute(path, isContainer, fileTypes);
+}, pathArgument, isContainerOption, fileTypesOption);
+
+await rootCommand.InvokeAsync(args);
---
+++ src/Kloc8Tests/ProgramTests.cs
@@ -0,0 +1,268 @@
+//namespace Kloc8Tests;
+
+//[TestClass]
+//public class ProgramTests
+//{
+//    private static string _rootPath; // ...\KLOC
+
+//    [ClassInitialize]
+//    public static void ClassInitialize(TestContext context)
+//    {
+//        // "...\KLOC\src\KlocTests\bin\Debug\KlocTests.dll" ==> "...\KLOC"
+//        _rootPath = Path.GetFullPath(
+//            Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location,
+//                @"..\..\..\..\..\..\"));
+//    }
+//    private static string GetPath(string relativePath)
+//    {
+//        return Path.Combine(_rootPath, relativePath);
+//    }
+
+//    private TextWriter _savedOutput;
+//    [TestInitialize]
+//    public void Initialize()
+//    {
+//        _savedOutput = Console.Out;
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+//    }
+//    [TestCleanup]
+//    public void Cleanup()
+//    {
+//        Console.SetOut(_savedOutput);
+//    }
+
+//    [TestMethod]
+//    public void Parameter_Missing()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC8.Program.Main(new string[0]);
+
+//        var output = console.GetStringBuilder().ToString();
+//        string line;
+//        using (var reader = new StringReader(output))
+//        {
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//        }
+//        Assert.AreEqual("Location of source code directory is missing.", line);
+//    }
+//    [TestMethod]
+//    public void Parameter_Wrong()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { "asdf" });
+
+//        var output = console.GetStringBuilder().ToString();
+//        string line;
+//        using (var reader = new StringReader(output))
+//        {
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//        }
+//        Assert.AreEqual("Location of source code directory does not exist.", line);
+//    }
+
+//    [TestMethod]
+//    public void File_UnknownType_markdown()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { GetPath("README.md") });
+
+//        var output = console.GetStringBuilder().ToString();
+//        string line = null;
+//        using (var reader = new StringReader(output))
+//        {
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//        }
+//        Assert.AreEqual("Not a source file.", line);
+//    }
+//    [TestMethod]
+//    public void File_UnknownType_dll()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { GetPath(@"src\KlocTests\bin\Debug\net5.0\KlocTests.dll") });
+
+//        var output = console.GetStringBuilder().ToString();
+//        string line = null;
+//        using (var reader = new StringReader(output))
+//        {
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//        }
+//        Assert.AreEqual("Not a source file.", line);
+//    }
+//    [TestMethod]
+//    public void File_Csharp()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { GetPath(@"src\KLOC\Program.cs") });
+
+//        var output = console.GetStringBuilder().ToString();
+//        var hasLine = false;
+//        using (var reader = new StringReader(output))
+//        {
+//            string line = null;
+//            while ((line = reader.ReadLine()) != null)
+//            {
+//                if (line.StartsWith("Source files:"))
+//                {
+//                    hasLine = true;
+//                    Assert.IsTrue(line.EndsWith(" 1"));
+//                    break;
+//                }
+//            }
+//        }
+//        Assert.IsTrue(hasLine);
+//    }
+//    [TestMethod]
+//    public void File_Project()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { GetPath(@"src\KLOC\KLOC.csproj") });
+
+//        var output = console.GetStringBuilder().ToString();
+//        var hasLine = false;
+//        using (var reader = new StringReader(output))
+//        {
+//            string line = null;
+//            while ((line = reader.ReadLine()) != null)
+//            {
+//                if (line.StartsWith("Projects:"))
+//                {
+//                    hasLine = true;
+//                    Assert.IsTrue(line.EndsWith(" 1"));
+//                    break;
+//                }
+//            }
+//        }
+//        Assert.IsTrue(hasLine);
+//    }
+//    [TestMethod]
+//    public void File_Solution()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { GetPath(@"src\KLOC.sln") });
+
+//        var output = console.GetStringBuilder().ToString();
+//        var hasLine = false;
+//        using (var reader = new StringReader(output))
+//        {
+//            string line = null;
+//            while ((line = reader.ReadLine()) != null)
+//            {
+//                if (line.StartsWith("Projects:"))
+//                {
+//                    hasLine = true;
+//                    Assert.IsTrue(line.EndsWith(" 2"));
+//                    break;
+//                }
+//            }
+//        }
+//        Assert.IsTrue(hasLine);
+//    }
+
+//    [TestMethod]
+//    public void Directory_NonExistent()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { GetPath(@"notexistentdirectory") });
+
+//        var output = console.GetStringBuilder().ToString();
+//        string line;
+//        using (var reader = new StringReader(output))
+//        {
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//            line = reader.ReadLine();
+//        }
+//        Assert.AreEqual("Location of source code directory does not exist.", line);
+//    }
+//    [TestMethod]
+//    public void Directory_GithubRepository()
+//    {
+//        Assert.Inconclusive();
+//        // _rootPath
+//    }
+//    [TestMethod]
+//    public void Directory_WithKnownFiles_Sln()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { GetPath(@"src") });
+
+//        var output = console.GetStringBuilder().ToString();
+//        var hasLine = false;
+//        using (var reader = new StringReader(output))
+//        {
+//            string line = null;
+//            while ((line = reader.ReadLine()) != null)
+//            {
+//                if (line.Contains(" .csproj "))
+//                {
+//                    hasLine = true;
+//                    Assert.IsTrue(line.EndsWith(" 2"));
+//                    break;
+//                }
+//            }
+//        }
+//        Assert.IsTrue(hasLine);
+//    }
+//    [TestMethod]
+//    public void Directory_WithKnownFiles_Csproj()
+//    {
+//        var console = new StringWriter();
+//        Console.SetOut(console);
+
+//        KLOC.Program.Main(new[] { GetPath(@"src\KLOC") });
+
+//        var output = console.GetStringBuilder().ToString();
+//        var hasLine = false;
+//        using (var reader = new StringReader(output))
+//        {
+//            string line = null;
+//            while ((line = reader.ReadLine()) != null)
+//            {
+//                if (line.Contains(" .csproj "))
+//                {
+//                    hasLine = true;
+//                    Assert.IsTrue(line.EndsWith(" 1"));
+//                    break;
+//                }
+//            }
+//        }
+//        Assert.IsTrue(hasLine);
+//    }
+//    [TestMethod]
+//    public void Directory_WithKnownFiles_Cs()
+//    {
+//        Assert.Inconclusive();
+//    }
+//}
