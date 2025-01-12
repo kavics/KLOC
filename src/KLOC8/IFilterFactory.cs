5f01177a50cca75aeb31850104abaeeafb308a8d
Zoltán Gyebrovszki <kavics@sensenet.com>
2025-01-12 16:26:32 +0100

    Add common klocignore option

---
+++ src/KLOC8/IFilterFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLOC8;
+
+public interface IFilterFactory
+{
+    IFilter CreateFilter(string? path);
+}
+
+public class FilterFactory(IDisk disk, IEnumerable<IFilter> filters) : IFilterFactory
+{
+    public IFilter CreateFilter(string? path)
+    {
+        Type type;
+        if (string.Compare(path, "builtin", StringComparison.InvariantCultureIgnoreCase) == 0)
+            type = typeof(CommonListFilter);
+        else if (path == null)
+            type = typeof(KlocIgnoreFileFilter);
+        else
+            type = typeof(GlobalKlocIgnoreFileFilter);
+
+        var filter = filters.FirstOrDefault(f => f.GetType() == type);
+        if(filter == null)
+            throw new InvalidOperationException($"Cannot create a filter.");
+
+        if(filter is GlobalKlocIgnoreFileFilter globalFilter)
+            globalFilter.SetFilterPath(path!);
+
+        return filter;
+    }
+}
